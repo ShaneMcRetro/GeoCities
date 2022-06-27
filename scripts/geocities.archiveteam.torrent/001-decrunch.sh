c6aaa5bc0213c52ddce4cc6b9c9c686e6b75a515
Shane McRetro <shanemcretro@gmail.com>
2022-06-27 15:07:19 +1000

    Update 001-decrunch.sh

--- scripts/geocities.archiveteam.torrent/001-decrunch.sh
+++ scripts/geocities.archiveteam.torrent/001-decrunch.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-# This script decrunches the archiveteam's Geocities torrent.
-# It is assumed that you kept the original structure and
-# file names after finishing the download.
+# This script decrunches ArchiveTeam's GeoCities Torrent.
+# https://wiki.archiveteam.org/index.php/GeoCities
+# It is assumed that you kept the original structure and filenames after finishing the download.
+# Make sure your environment variables are set as per README at https://github.com/ShaneMcRetro/GeoCities
 
 cd $GEO_SOURCE/geocities.archiveteam.torrent/
 
-# File contains zeroes.
+# Remove files that contain nothing but zeroes.
 sudo rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES/ghiageocities.com.7z.001
-# SUBSITES version contains zeroes. Uncorrupted version in YAHOO subfolder.
 sudo rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES/mx.geocities.yahoo.com.7z.001
-# Files containing nothing but zeroes and have a bonus malformed forward slash (%2F)
 sudo rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES%2Fghiageocities.com.7z.001
 sudo rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES%2Fmx.geocities.yahoo.com.7z.001
 sudo rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES%2Fghiageocities.com.7z.001_meta.txt
 sudo rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES%2Fmx.geocities.yahoo.com.7z.001_meta.txt
 
-# Find all 7z archives and decrunch them. The option -P4 means
-# "run 4 7zip decrunchers at the same time". Adjust the number to
-# the number of cores your processor has.
-# -aou flag renames extracted file if a duplicated with an _1 (or _2 if double duplicated!).
+# Find all 7z archives and decrunch them.
+# The option -P4 means "run 4 7zip decrunchers at the same time".
+# Adjust the number to the number of cores your processor has.
+# If a duplicate file is extracted the -aou flag appends _1 to the extracted file.
 
 find . -name \*.7z.001 | xargs -P4 -I filename 7z x -aou filename -o$GEO_WORK |& tee -a $GEO_LOGS/001-decrunch.log
 
-# Remove decrunched duplicates, where did they come from? I do not know...
+# Remove decrunched duplicates, where did they come from? I do not know but those md5s match so rm, rm, rm.
 cd $GEO_WORK
 sudo rm geocities.yahoo_1.com geocities.yahoo.co_1.jp ar.geocities.yahoo_1.com uk.geocities.yahoo_1.com us.geocities.yahoo_1.com visit.geocities.yahoo_1.com themis.geocities.yahoo_1.com
 
@@ -31,7 +30,3 @@ sudo rm geocities.yahoo_1.com geocities.yahoo.co_1.jp ar.geocities.yahoo_1.com u
 # This creates duplicates files, why was this here? Confirm /media/shane/GeoCities2TB/work/geocities/geocities.com/1christlover exists with one index.html
 # 7z x -aou $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES/geocities.com.7z.001 -o$GEO_WORK  |& tee $GEO_LOGS/001-decrunch_extra.log
 # If confirmed to exist, delete this block.
-
-#real    361m21.098s
-#user    799m53.967s
-#sys     15m55.676s
