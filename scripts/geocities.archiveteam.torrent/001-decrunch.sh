fb1afd3b44935405c14b1f6f1a48897692db0416
Shane McRetro <shanemcretro@gmail.com>
2022-06-27 14:44:08 +1000

    Update 001-decrunch.sh

--- scripts/geocities.archiveteam.torrent/001-decrunch.sh
+++ scripts/geocities.archiveteam.torrent/001-decrunch.sh
@@ -6,18 +6,13 @@
 
 cd $GEO_SOURCE/geocities.archiveteam.torrent/
 
-# RESYNC THE GEO_SOURCE TO CHECK THIS
-# Is geocities.com.7z.001 the same as geocities.com.7z.001.part?
-# Remove partial or empty downloads - EXTRACT AND COMPARE FIRST, are they unique?
-# sudo rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES/geocities.com.7z.001.part |& tee $GEO_LOGS/001-decrunch1.log
-
 # File contains zeroes.
 sudo rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES/ghiageocities.com.7z.001
 # SUBSITES version contains zeroes. Uncorrupted version in YAHOO subfolder.
 sudo rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES/mx.geocities.yahoo.com.7z.001
-#CORRUPTED ROOT FILES WITH SIMILAR NAMES AS ABOVE (the ones with percentage signs)
-sudo
-sudo 
+# Files containing nothing but zeroes and have a bonus malformed forward slash (%2F)
+sudo rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES%2Fghiageocities.com.7z.001
+sudo rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES%2Fmx.geocities.yahoo.com.7z.001
 
 
 # Find all 7z archives and decrunch them. The option -P4 means
@@ -34,6 +29,7 @@ sudo rm geocities.yahoo_1.com geocities.yahoo.co_1.jp ar.geocities.yahoo_1.com u
 # ON THE NEXT RUN
 # This creates duplicates files, why was this here? Confirm /media/shane/GeoCities2TB/work/geocities/geocities.com/1christlover exists with one index.html
 # 7z x -aou $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES/geocities.com.7z.001 -o$GEO_WORK  |& tee $GEO_LOGS/001-decrunch_extra.log
+# If confirmed to exist, delete this block.
 
 #real    361m21.098s
 #user    799m53.967s
