b14f38e9c044f4cf098c990d7b1edf568a51fc56
Shane McRetro <shanemcretro@gmail.com>
2022-06-27 15:56:38 +1000

    Update 001-decrunch.sh

--- scripts/geocities.archiveteam.torrent/001-decrunch.sh
+++ scripts/geocities.archiveteam.torrent/001-decrunch.sh
@@ -5,7 +5,7 @@
 # It is assumed that you kept the original structure and filenames after finishing the download.
 # Make sure your environment variables are set as per README at https://github.com/ShaneMcRetro/GeoCities
 
-	cd $GEO_SOURCE/geocities.archiveteam.torrent/
+cd $GEO_SOURCE/geocities.archiveteam.torrent/
 
 # Remove files that contain nothing but zeroes.
 sudo rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES/ghiageocities.com.7z.001
@@ -20,13 +20,12 @@ sudo rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES%2Fmx.geocities.yahoo.
 # Adjust the number to the number of cores your processor has.
 # If a duplicate file is extracted the -aou flag appends _1 to the extracted file.
 
-  find . -name \*.7z.001 | xargs -P4 -I filename 7z x -aou filename -o$GEO_WORK |& tee -a $GEO_LOGS/001-decrunch.log
+find . -name \*.7z.001 | xargs -P4 -I filename 7z x -aou filename -o$GEO_WORK |& tee -a $GEO_LOGS/001-decrunch.log
 
 # Remove decrunched duplicates, where did they come from? I do not know but those MD5s do match so... rm, rm, rm.
 
-  cd $GEO_WORK
-  
-  sudo rm geocities.yahoo_1.com geocities.yahoo.co_1.jp ar.geocities.yahoo_1.com uk.geocities.yahoo_1.com us.geocities.yahoo_1.com visit.geocities.yahoo_1.com themis.geocities.yahoo_1.com
+cd $GEO_WORK
+sudo rm geocities.yahoo_1.com geocities.yahoo.co_1.jp ar.geocities.yahoo_1.com uk.geocities.yahoo_1.com us.geocities.yahoo_1.com visit.geocities.yahoo_1.com themis.geocities.yahoo_1.com
 
 # ON THE NEXT RUN
 # This creates duplicates files, why was this here? Confirm /media/shane/GeoCities2TB/work/geocities/geocities.com/1christlover exists with one index.html
