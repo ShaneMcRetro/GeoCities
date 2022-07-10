51a7bfc5d881070bcb6f16e0a337a977a34db88e
Shane McRetro <shanemcretro@gmail.com>
2022-07-10 22:13:33 +1000

    Update 005-merge-geocities-www-geocities.sh

--- scripts/geocities.archiveteam.torrent/005-merge-geocities-www-geocities.sh
+++ scripts/geocities.archiveteam.torrent/005-merge-geocities-www-geocities.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-# The $GEO_WORK folder :
-# Should be ~39,507,391 items, totalling 911.0 GB via properties in GNOME.
+# The $GEO_WORK folder:
+# Should contain ~39,507,391 items, totalling 911.0 GB via properties in GNOME.
 
-# The entire drive should report 994 GB used, 873.8 GB free via disk properties (Alt-Return in root)
+# The entire drive should report 994 GB used, 873.8 GB free via disk properties (Alt-Return as root)
 # df -h reports size as 1.8T, 926G used, 814G free.
 # df -H reports size as 2.0T, 994G used, 874G free.
 
-# Merge geocities.com into www.geocities.com
+# Merge geocities.com folders into www.geocities.com
 
 cd $GEO_WORK/geocities/
 $GEO_SCRIPTS/merge_directories.pl geocities.com www.geocities.com
 
-# save conflicts
+# Save conflicts
 mkdir -p $GEO_WORK/geocities_conflicts_1/www.geocities.com
 $GEO_SCRIPTS/merge_directories.pl geocities.com $GEO_WORK/geocities_conflicts_1/www.geocities.com
 
 # One this has run you will need to do modify the above to do the same to JCN, Latecomer, Santiago (and unmangled-SpindleyQ?)
 # Get everything that isn't the main archive (above) into a single www.geocities.com folder then merge it into the main archive.
+# See 005a-more-merging.
