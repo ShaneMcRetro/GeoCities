3bdd37615ed1f01ba13235279ac5c511b2e330d9
Shane McRetro <hello@mcretro.net>
2022-11-17 08:18:59 +1100

    Update 003-remove-symlinks.sh

--- scripts/geocities.archiveteam.torrent/003-remove-symlinks.sh
+++ scripts/geocities.archiveteam.torrent/003-remove-symlinks.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Symlinks are a PITA, so remove them all.
-# Anyway it is already known where belongs what:
-# All the user directories from YAHOOIDS need to
-# go to www.geocities.com ... the rest just tries
-# to solve case issues that will be tackled
-# in another way soon.
-
-# Note to future self, do not try and do this while fighting the flu.
-# Decompress and move JCN, Latecomer and Santiago work files into $GEO_WORK in their own respective subfolders.
+# Symlinks are bad for business, so we'll remove them all.
+# Anyway, it is already known what belongs where:
+# All user directories from YAHOOIDS need to go to www.geocities.com.
+# The rest just tries to solve cAsE iSsUeS that will be tackled in another way soon.
 
 cd $GEO_WORK
 
 find . -type l -print0  |  xargs -n 1 -P4 -0 rm -v
 # "type l" means           Delete these bastards...
 # to look for symlinks.    fast!
+
+
+
+
+# TIMES GO HERE
