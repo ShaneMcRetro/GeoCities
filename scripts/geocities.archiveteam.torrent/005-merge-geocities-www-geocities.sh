4d1cb43b952da5b2f64acd15738a454aca0f000b
Shane McRetro <hello@mcretro.net>
2022-11-18 21:34:53 +1100

    Update 005-merge-geocities-www-geocities.sh

--- scripts/geocities.archiveteam.torrent/005-merge-geocities-www-geocities.sh
+++ scripts/geocities.archiveteam.torrent/005-merge-geocities-www-geocities.sh
@@ -10,14 +10,15 @@ cd $GEO_WORK/geocities/
 $GEO_SCRIPTS/merge_directories.pl geocities.com www.geocities.com
 
 # Save conflicts, this will also delete the geocities.com folder once it is empty.
-# The contents of geocities.com that could not be merged are now considered conflicting folders and files.
+# The contents of geocities.com that could not be merged are now considered conflicting.
 
-mkdir -p $GEO_WORK/geocities_conflicts_1/www.geocities.com
-$GEO_SCRIPTS/merge_directories.pl geocities.com $GEO_WORK/geocities_conflicts_1/www.geocities.com
+mkdir -p $GEO_WORK/conflicts/geocities_conflicts_1/www.geocities.com
+$GEO_SCRIPTS/merge_directories.pl geocities.com $GEO_WORK/conflicts/geocities_conflicts_1/www.geocities.com
 
 # One this has run you will need to do modify the above to do the same to JCN, Latecomer, Santiago (and unmangled-SpindleyQ?)
 # Get everything that isn't the main archive (above) into a single www.geocities.com folder then merge it into the main archive.
 # Plus delocalise the regional subsites to bring all of GeoCities back under one umbrella.
 
-# TIMES
-
+# real  5m43.016s
+# user  3m6.818s
+# sys   2m37.603s
