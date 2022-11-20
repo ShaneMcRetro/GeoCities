0221ffe83f389dc79f7bbd7f02078dd22a395413
Shane McRetro <hello@mcretro.net>
2022-11-20 17:06:37 +1100

    Destabilizing Economic Indicators

--- scripts/geocities.archiveteam.torrent/005-merge-geocities-www-geocities.sh
+++ scripts/geocities.archiveteam.torrent/005-merge-geocities-www-geocities.sh
@@ -10,7 +10,8 @@ cd $GEO_WORK/geocities/
 $GEO_SCRIPTS/merge_directories.pl geocities.com www.geocities.com
 
 # Save conflicts, this will also delete the geocities.com folder once it is empty.
-# The contents of geocities.com that could not be merged are now considered conflicting.
+# The contents of geocities.com that could not be merged are now considered conflicts.
+# We save this data for processing in the database later on.
 
 mkdir -p $GEO_WORK/conflicts/main1/www.geocities.com
 $GEO_SCRIPTS/merge_directories.pl geocities.com $GEO_WORK/conflicts/main1/www.geocities.com
@@ -18,6 +19,7 @@ $GEO_SCRIPTS/merge_directories.pl geocities.com $GEO_WORK/conflicts/main1/www.ge
 # Once this has run you will need to modify the above to do the same to JCN, Latecomer, Santiago (and unmangled-SpindleyQ?)
 # Get everything that isn't the main archive (above) into a single www.geocities.com folder then merge it into the main archive.
 # Plus delocalise the regional subsites to bring all of GeoCities under one umbrella.
+
 
 # real  5m43.016s
 # user  3m6.818s
