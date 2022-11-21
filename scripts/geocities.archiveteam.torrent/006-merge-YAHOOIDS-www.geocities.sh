0b2759636ff86c306c391f0c4731ccb755e4b451
Shane McRetro <hello@mcretro.net>
2022-11-21 19:59:57 +1100

    Manipulating Modal Memory

--- scripts/geocities.archiveteam.torrent/006-merge-YAHOOIDS-www.geocities.sh
+++ scripts/geocities.archiveteam.torrent/006-merge-YAHOOIDS-www.geocities.sh
@@ -12,13 +12,17 @@ mkdir -p $GEO_WORK/conflicts/main2/www.geocities.com
 find . -mindepth 2 -maxdepth 2 -type d -print0 | xargs -0 -I pathname bash -c '$GEO_SCRIPTS/merge_directories.pl pathname $GEO_WORK/conflicts/main2/www.geocities.com'
 
 
-# Manually delete these after inspecting to see what's left after two rounds.
-# rm -rv $GEO_WORK/geocities/YAHOOIDS
+# Remove the empty folders
+rm -rv $GEO_WORK/geocities/YAHOOIDS
+
+# The $GEO_WORK drive should report via df -H:
+# Filesystem  Size  Used  Avail  Use%  Mounted on
+# /dev/sdb    2.0T  924G  945G   50%   /media/ubuntu/GC_2TB       # aka $GEO_WORK
+
+# real  1534m7.038s
+# user  858m28.198s
+# sys   633m21.002s
+
 
 # Now that that is out of the way do a full backup.
-
 # sudo dd if=/dev/sdX of=/dev/sdX bs=4M status=progress
-
-# real    1909m46.145s
-# user    46m57.052s
-# sys     141m25.910s
