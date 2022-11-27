c47e4b868a0947abc20713c28a53ccd7641248fd
Shane McRetro <hello@mcretro.net>
2022-11-27 22:32:59 +1100

    Update 010-symlink-cancer.sh

--- scripts/geocities.archiveteam.torrent/010-symlink-cancer.sh
+++ scripts/geocities.archiveteam.torrent/010-symlink-cancer.sh
@@ -8,6 +8,7 @@
 # To find them, the SQL query in $GEO_SCRIPTS/sql/do/find-doubles.sql was used.
 # This is based on the doubles-dir data generated in 009-case-insensitivity-dirs.sh
 
+
 rm -rv $GEO_WORK/geocities/www.geocities.com/Tokyo/Temple/2506/pisces/beth/beth
 
 rm -rv $GEO_WORK/geocities/www.geocities.com/Wellesley/Veranda/9569/index/harvest/harvest
