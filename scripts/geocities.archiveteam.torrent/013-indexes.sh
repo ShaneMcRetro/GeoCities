c8daba2eb8a59eb64d505c1131fb9dee2cc24674
Shane McRetro <hello@mcretro.net>
2022-12-01 02:31:42 +1100

    Update 013-indexes.sh

--- scripts/geocities.archiveteam.torrent/013-indexes.sh
+++ scripts/geocities.archiveteam.torrent/013-indexes.sh
@@ -1,13 +1,11 @@
-# Find everything that could be an index file,
-# like "index.html", "index.htm", "INDEX.HTM", etc
+# Find everything that could be an index file, like "index.html", "index.htm", "INDEX.HTM", etc.
 
 psql -d $GEO_DB_DB --no-align --tuples-only -U despens -f $GEO_SCRIPTS/sql/do/find-indexes.sql -o $GEO_LOGS/indexes-list.txt
 
 # real    3m34.571s
 # user    0m0.056s
-# sys 0m0.024s
+# sys     0m0.024s
 
-# Delete unneeded index files from disk and database,
-# mark the right ones as "homepage".
+# Delete unneeded index files from disk and database, mark the right ones as "homepage".
 
 $GEO_SCRIPTS/filter-indexes.pl
