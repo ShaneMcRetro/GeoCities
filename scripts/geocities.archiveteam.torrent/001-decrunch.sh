c293d0401842713792c9235666a2663fb9fa638d
Shane McRetro <hello@mcretro.net>
2022-12-16 14:32:20 +1100

    Update 001-decrunch.sh

--- scripts/geocities.archiveteam.torrent/001-decrunch.sh
+++ scripts/geocities.archiveteam.torrent/001-decrunch.sh
@@ -3,7 +3,7 @@
 # This script decrunches ArchiveTeam's GeoCities Torrent.
 # https://wiki.archiveteam.org/index.php/GeoCities
 # It is assumed that you kept the original structure and filenames after finishing the download.
-# Make sure your environment variables are set as per 000a-setup.txt at https://github.com/ShaneMcRetro/GeoCities
+# Make sure your environment variables are set as per 000a-setup.sh at https://github.com/ShaneMcRetro/GeoCities
 
 cd $GEO_SOURCE/geocities.archiveteam.torrent/
 
@@ -16,26 +16,33 @@ rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES%2Fmx.geocities.yahoo.com.7
 rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES%2Fghiageocities.com.7z.001_meta.txt
 rm $GEO_SOURCE/geocities.archiveteam.torrent/SUBSITES%2Fmx.geocities.yahoo.com.7z.001_meta.txt
 
-# Find all 7z archives and decrunch them to $GEO_WORK
+# Find all 7z archives and decrunch them to $GEO_WORK.
 # The option -P8 means "run eight 7zip decrunchers at the same time".
 # Adjust the number to the number of cores (or threads) your processor has.
 # If a duplicate file is extracted the -aou flag appends _1 to the extracted file.
 
 find . -name \*.7z.001 | xargs -P8 -I filename 7z x -aou filename -o$GEO_WORK |& tee -a $GEO_LOGS/001-decrunch.log
 
-# Remove some decrunched duplicates. Their MD5s match so we will remove them.
+# Tidy up and remove some decrunched duplicates, where did they come from? 001, but their MD5s match so we will remove them.
 
 cd $GEO_WORK
-rm -rv geocities.yahoo_1.com
-rm -rv geocities.yahoo.co_1.jp
-rm -rv ar.geocities.yahoo_1.com
-rm -rv uk.geocities.yahoo_1.com
-rm -rv us.geocities.yahoo_1.com
-rm -rv visit.geocities.yahoo_1.com
-rm -rv themis.geocities.yahoo_1.com
+rm -rv $GEO_WORK/geocities.yahoo_1.com
+rm -rv $GEO_WORK/geocities.yahoo.co_1.jp
+rm -rv $GEO_WORK/ar.geocities.yahoo_1.com
+rm -rv $GEO_WORK/uk.geocities.yahoo_1.com
+rm -rv $GEO_WORK/us.geocities.yahoo_1.com
+rm -rv $GEO_WORK/visit.geocities.yahoo_1.com
+rm -rv $GEO_WORK/themis.geocities.yahoo_1.com
 
 # Check the $GEO_LOG/001-decrunch.log for any errors encountered (search keyword "error" or "fail", probably)
 
-# real  249m14.277s
-# user  894m58.437s
-# sys   27m46.601s
+# Ubuntu 12.04
+# real    464m28.728s
+# user    983m8.043s
+# sys     23m14.633s
+
+
+# df -H on $GEO_WORK
+# Filesystem      Size  Used  Avail  Use%  Mounted on
+# /dev/sdb        2.0T  910G  960G   49%  /media/GC_2TB
+
