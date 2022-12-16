b030373455470f73c312e560e992b1eb2fb807ca
Shane McRetro <hello@mcretro.net>
2022-12-16 14:27:19 +1100

    Update 006-merge-YAHOOIDS-www.geocities.sh

--- scripts/geocities.archiveteam.torrent/006-merge-YAHOOIDS-www.geocities.sh
+++ scripts/geocities.archiveteam.torrent/006-merge-YAHOOIDS-www.geocities.sh
@@ -7,7 +7,7 @@ find . -mindepth 2 -maxdepth 2 -type d -print0 | xargs -0 -I pathname bash -c '$
 
 find . -mindepth 2 -maxdepth 2 -type d -print0 | xargs -0 -I pathname bash -c '$GEO_SCRIPTS/merge_directories.pl pathname $GEO_WORK/geocities_conflicts_1/www.geocities.com'
 
-mkdir -p $GEO_WORK/conflicts/main2/www.geocities.com
+mkdir -p $GEO_WORK/geocities_conflicts_2/www.geocities.com
 
 find . -mindepth 2 -maxdepth 2 -type d -print0 | xargs -0 -I pathname bash -c '$GEO_SCRIPTS/merge_directories.pl pathname $GEO_WORK/geocities_conflicts_2/www.geocities.com'
 
@@ -15,14 +15,15 @@ find . -mindepth 2 -maxdepth 2 -type d -print0 | xargs -0 -I pathname bash -c '$
 # Remove the empty folders
 rm -rv $GEO_WORK/geocities/YAHOOIDS
 
+
+# Ubuntu 12.04
+# real	1649m47.320s
+# user	256m34.022s
+# sys	1384m23.223s
+
 # The $GEO_WORK drive should report via df -H:
 # Filesystem  Size  Used  Avail  Use%  Mounted on
 # /dev/sdb    2.0T  924G  945G   50%   /media/ubuntu/GC_2TB       # aka $GEO_WORK
-
-# real  1534m7.038s
-# user  858m28.198s
-# sys   633m21.002s
-
 
 # Now that that is out of the way do a full backup.
 # sudo dd if=/dev/sdX of=/dev/sdX bs=4M status=progress
